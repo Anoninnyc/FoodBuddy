be0f93ae499d87799664daea7abed94710f3c81c
Krishan Arya <krishanmarya1@gmail.com>
2016-10-03 23:41:36 +1100

    Update render

--- public/index.jsx
+++ public/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from './components/App'
+import  App  from './components/App'
 import injectTapEventPlugin from 'react-tap-event-plugin';
 injectTapEventPlugin();
 
