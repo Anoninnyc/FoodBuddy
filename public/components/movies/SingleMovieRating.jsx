2493710fe51fd353c50cecc6a500c90fb634b4d5
Krishan Arya <krishanmarya1@gmail.com>
2016-10-04 03:13:21 +1100

    Modulate

--- public/components/movies/SingleMovieRating.jsx
+++ public/components/movies/SingleMovieRating.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import SingleMovieRatingEntry from './SingleMovieRatingEntry'
 import StarRatingComponent from './StarRatingComponent'
 import ReviewComponent from './ReviewComponent'
-
+//
 
 
 class SingleMovieRating extends Component {
