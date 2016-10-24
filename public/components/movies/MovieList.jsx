7889e611695c5deeca5b38769e5b89547e45d51c
Krishan Arya <krishanmarya1@gmail.com>
2016-10-24 21:08:44 +1100

    Continue_to_lint

--- public/components/movies/MovieList.jsx
+++ public/components/movies/MovieList.jsx
@@ -1,13 +1,15 @@
-import React, { Component } from 'react'
-import MovieListEntry from './MovieListEntry'
+import React, { Component } from 'react';
+import MovieListEntry from './MovieListEntry';
 
 const MovieList = ({movies, change, view}) => (
 	<div className='movieList'>
-		{ movies.map((movie, i) => <MovieListEntry
-      movie = {movie} 
-      change = {change}
-      key = {movie.title} 
-      /> )}
+	  { movies.map((movie, i) => 
+		<MovieListEntry
+	      movie = {movie}
+	      change = {change}
+	      key = {movie.title}
+	    /> 
+	  )}
 	</div>
 );
 
