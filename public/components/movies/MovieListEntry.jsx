cb78d8a19e203db2bc36ea5b95e848370306b388
Krishan Arya <krishanmarya1@gmail.com>
2016-10-25 11:53:52 +1100

    Ping_server

--- public/components/movies/MovieListEntry.jsx
+++ public/components/movies/MovieListEntry.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import ReviewComponent from './ReviewComponent'
-import MovieWatchRequest from './MovieWatchRequest'
-import StarRatingComponent from './StarRatingComponent'
+import ReviewComponent from './ReviewComponent';
+import MovieWatchRequest from './MovieWatchRequest';
+import StarRatingComponent from './StarRatingComponent';
 
 
 class MovieListEntry extends Component {
