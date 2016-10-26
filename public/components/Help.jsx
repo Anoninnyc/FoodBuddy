c357b6a3a8f5fa8bcccc64f9bdf890e0f80b245b
Krishan Arya <krishanmarya1@gmail.com>
2016-10-26 17:55:33 +1100

    Update_help

--- public/components/Help.jsx
+++ public/components/Help.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
-
 const Help = () => (
-	<div id='helpInfo'>
-	  This should be help
-	</div>
+  <div id='helpInfo'>
+	<h1 id="helpIntro"> How to use this app</h1>
+	  - Click the "Home" tab to view recent releases, and to search for specific movies. 
+	  - You can rate movies, leave reviews, or invite friends to watch it with you. 
+	  - Click the "My Ratings" tab to view the movies you've rated. 
+	  - To view friends you can invite, go to the "New Buddies" tab and send out invites.
+	  (To view your current friends, click the "My Friends" tab)
+	  - To view any friend requests, or requests from friends to watch movies. 
+  </div>
 );
 
 export default Help;
