db6786da3a5b93aaff866840c4d1d1f6631419b7
Krishan Arya <krishanmarya1@gmail.com>
2016-10-26 17:44:03 +1100

    Add_help

--- public/components/Help.jsx
+++ public/components/Help.jsx
@@ -1,16 +1,10 @@
 import React, { Component } from 'react';
 
 
-const MovieList = ({movies, change, view}) => (
-	<div className='movieList'>
-	  { movies.map((movie, i) => 
-		<MovieListEntry
-	      movie = {movie}
-	      change = {change}
-	      key = {movie.title}
-	    /> 
-	  )}
+const Help = () => (
+	<div id='helpInfo'>
+	  This should be help
 	</div>
 );
 
-export default MovieList;
+export default Help;
