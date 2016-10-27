4c5d4ece0a235d5072ebfac598fc0fa81c632b7c
Krishan Arya <krishanmarya1@gmail.com>
2016-10-27 11:17:13 +1100

    Begin_to_refactor_route

--- public/components/Help.jsx
+++ public/components/Help.jsx
@@ -5,9 +5,11 @@ const Help = () => (
 	<h1 id="helpIntro"> How to use this App</h1>
 	  <div>Click the "Home" tab to view recent releases, and to search for specific movies. </div>
 	  <div>You can rate movies, leave reviews, or invite friends to watch it with you. </div>
+	  <br/>
 	  <div>Click the "My Ratings" tab to view the movies you've rated. </div>
 	  <div>To view friends you can invite, go to the "New Buddies" tab and send out invites.</div>
 	  <div>(To view your current friends, click the "My Friends" tab)</div>
+	  <br/>
 	 <div> To view any friend requests, or requests from friends to watch movies. </div>
   </div>
 );
