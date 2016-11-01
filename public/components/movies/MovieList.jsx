f6461492f37122f6776aef5fda8d28326318421b
Krishan Arya <krishanmarya1@gmail.com>
2016-11-02 01:28:36 +1100

    Update_whence

--- public/components/movies/MovieList.jsx
+++ public/components/movies/MovieList.jsx
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 import MovieListEntry from './MovieListEntry';
 
-const MovieList = ({movies, change, view}) => (
+const MovieList = ({movies, change, view, whence}) => (
 	<div className='movieList'>
-	  { movies.map((movie, i) => 
+	{movies.map((movie, i) =>
 		<MovieListEntry
-	      movie = {movie}
-	      change = {change}
-	      key = {movie.title}
-	    /> 
+	      movie={movie}
+	      change={change}
+	      key={movie.title}
+	      whence={whence}
+	    />
 	  )}
 	</div>
 );
