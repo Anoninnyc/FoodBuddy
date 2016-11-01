cefe9534abe67d2975640add678b860e45ce042d
Krishan Arya <krishanmarya1@gmail.com>
2016-11-02 02:00:39 +1100

    Update_whence

--- public/components/movies/MovieListEntry.jsx
+++ public/components/movies/MovieListEntry.jsx
@@ -39,7 +39,7 @@ class MovieListEntry extends Component {
 
   	return (
   		<div className='movieEntry collection-item row'>
-  			<img className='moviethumnail col s3' src={movie.poster} onClick={() => (this.props.change("SingleMovie", movie, this.props.myRatings, this.props.whence))} alt="no_image_available.gif"/>
+  			<img className='moviethumnail col s3' src={movie.poster} onClick={() => { console.log("whence!!", this.props.whence) , this.props.change("SingleMovie", movie, this.props.myRatings, this.props.whence)}} alt="no_image_available.gif"/>
         <div className='right col s9'>
     			<h5 className='movieTitle' onClick={() => (this.props.change("SingleMovie", movie))}>{movie.title}</h5>
     			<p className='movieYear'>{movie.release_date}</p>
