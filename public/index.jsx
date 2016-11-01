c4a5352db05246eb0f01e7f22a9cc00dccc51117
Krishan Arya <krishanmarya1@gmail.com>
2016-11-02 10:01:28 +1100

    Update_routes

--- public/index.jsx
+++ public/index.jsx
@@ -1,12 +1,14 @@
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App';
 import { Router, browserHistory } from 'react-router';
+import routes from './routes';
+
 
 injectTapEventPlugin();
 
-render(<App />,
-  document.getElementById('app')
-);
+
+render((
+  <Router history={browserHistory} routes={routes} />
+), document.getElementById('app'));
 
