2e57bf9b111bb7025fe4b48bc0669e1afd6047b3
Krishan Arya <krishanmarya1@gmail.com>
2016-11-02 12:02:31 +1100

    React_route

--- public/components/Nav.jsx
+++ public/components/Nav.jsx
@@ -8,15 +8,15 @@ const Nav = ({onClick, find, logout, name}) => (
           <div className="nav-wrapper">
             <a href="#" onClick={() => (onClick("Home"))} className="brand-logo center">ReelPals</a>
             <ul id="nav-mobile" className="left hide-on-med-and-down">
-              <Link to="/home"><li><a onClick={() => (onClick("Home"))}>Home</a></li></Link>
-               <Link to="/MyRatings"><li><a onClick={() => (onClick("MyRatings"))}>My Ratings</a></li></Link>
-                <Link to="/Friends"><li><a onClick={() => (onClick("Friends"))}>My Friends</a></li></Link>
-               <Link to="/FindMovieBuddy"><li><a onClick={find}>New Buddies</a></li></Link>
+              <Link to="/home"><li onClick={() => (onClick("Home"))}>Home</li></Link>
+               <Link to="/MyRatings"><li onClick={() => (onClick("MyRatings"))}>My Ratings</li></Link>
+                <Link to="/Friends"><li onClick={() => (onClick("Friends"))}>My Friends</li></Link>
+               <Link to="/FindMovieBuddy"><li onClick={find}>New Buddies</li></Link>
             </ul>
             <ul id="nav-mobile" className="right hide-on-med-and-down">
-               <Link to="/inbox"><li><a onClick={() => (onClick("Inbox"))}>Notifications</a></li></Link>
-               <Link to="/help"><li><a onClick={() => (onClick("Help"))}>Help!</a></li></Link>
-               <Link to="/signup"><li><a onClick={logout}>Log Out</a></li></Link>
+               <Link to="/inbox"><li onClick={() => (onClick("Inbox"))}>Notifications</li></Link>
+               <Link to="/help"><li onClick={() => (onClick("Help"))}>Help!</li></Link>
+               <Link to="/signup"><li onClick={logout}>Log Out</li></Link>
             </ul>
           </div>
         </nav>
